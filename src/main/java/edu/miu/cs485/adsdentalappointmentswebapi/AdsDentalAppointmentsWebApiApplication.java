256e8c9da3b3b416cd3ae8cadeeae12a3ff9d28f
chinawutao <462755350@qq.com>
2023-10-25 13:38:45 -0500

    Initial commit

---
+++ src/main/java/edu/miu/cs485/adsdentalappointmentswebapi/AdsDentalAppointmentsWebApiApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs485.adsdentalappointmentswebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsDentalAppointmentsWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdsDentalAppointmentsWebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/miu/cs485/adsdentalappointmentswebapi/AdsDentalAppointmentsWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs485.adsdentalappointmentswebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsDentalAppointmentsWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
